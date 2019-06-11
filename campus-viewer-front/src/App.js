161accad874039a3c9705b72a1cf60690b544ec7
Stephen <stephenjk@optonline.net>
2019-06-11 11:43:44 -0400

    new forms and edit forms

--- campus-viewer-front/src/App.js
+++ campus-viewer-front/src/App.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Home from './components/Home'
 /***/
+import Home from './components/Home'
 import Students from './components/Students'
 import Campuses from './components/Campuses'
+import NewStudent from './components/NewStudentForm'
+import NewCampus from './components/NewCampusForm'
+import EditStudent from './components/EditStudentForm'
+import EditCampus from './components/EditCampusForm'
 /***/
 
 let campusArray=[
@@ -46,14 +50,39 @@ class App extends Component {
 
     const CampusesComponent = () => (<Campuses campusArray={campusArray} />)
 
+    const NewStudentComponent = () => (<NewStudent newStudent/> )
+
+    const NewCampusComponent = () => (<NewCampus newCampus/> )
+
+    const EditStudentComponent = () => (<EditStudent editStudent/> )
+
+    const EditCampusComponent = () => (<EditCampus editCampus/> )
+
     return (
       <Router>
         <Switch>
+          {/* Home
+            link to all campuses, all students */}
           <Route exact path="/" component={HomeComponent}/>
-        [// all students]
-          <Route exact path="/students" component={StudentsComponent}/>
-        [// all campuses]
-          <Route exact path="/campuses" component={CampusesComponent}/>
+
+          {/*all students
+            displays students*/}
+          <Route path="/students" component={StudentsComponent}/>
+
+          {/* all campuses
+            displays campuses, add student */}
+          <Route path="/campuses" component={CampusesComponent}/>
+
+          <Route path="/newstudent" component={NewStudentComponent}/>
+
+          <Route path="/newcampus" component={NewCampusComponent}/>
+
+          <Route path="/editstudent" component={EditStudentComponent}/>
+
+          <Route path="/editcampus" component={EditCampusComponent}/>
+
+
+
         </Switch>
       </Router>
       );
