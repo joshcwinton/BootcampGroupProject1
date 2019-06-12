5980845ab1578189a7669a5d6be1db4ae46a7b99
joshcwinton <joshcwinton@gmail.com>
2019-06-11 23:27:28 -0400

    added a couple of postgres functions, not changed in frontend, issue with results.insertId

--- campus-viewer-back/app.js
+++ campus-viewer-back/app.js
@@ -1,86 +1,89 @@
-var express = require('express');
-var morgan = require('morgan');
-var parser = require('body-parser');
+const express = require('express')
+const morgan = require('morgan')
+const parser = require('body-parser')
+const db = require('./queries')
+const app = express()
 
-var app = express();
+app.use(morgan("dev"))
+app.use(parser.json())
+app.use(
+  parser.urlencoded({
+    extended: true,
+  })
+)
+//
+// let campusArray=[
+//     {
+//         name: "Campus 1",
+//         image: "campus1.jpg",
+//         population: 1,
+//         id: 1
+//     },
+//     {
+//         name: "Campus 2",
+//         image: "campus2.jpg",
+//         population: 2,
+//         id: 2
+//     },
+//     {
+//         name: "Campus 3",
+//         image: "campus3.jpg",
+//         population: 2,
+//         id: 3
+//     },
+// ]
+//
+// let studentsArray=[
+//     {
+//         name: "Belinda",
+//         id: 1
+//     },
+//     {
+//         name: "Josh",
+//         id: 2
+//     },
+//     {
+//         name: "Stephen",
+//         id: 3
+//     }
+// ]
+//
+// deleteCampus = (campusName, campusArray) => {
+//   let index = -1
+//   for (let i = 0 i < campusArray.length i++) {
+//     if (campusArray[i].name == campusName) {
+//       index = i
+//     }
+//   }
+//   if (index > -1) {
+//     campusArray.splice(index, 1)
+//   }
+//   console.log(campusArray)
+// }
 
-app.use(morgan("dev"));
-app.use(parser.json());
-
-let campusArray=[
-    {
-        name: "Campus 1",
-        image: "campus1.jpg",
-        population: 1,
-        id: 1
-    },
-    {
-        name: "Campus 2",
-        image: "campus2.jpg",
-        population: 2,
-        id: 2
-    },
-    {
-        name: "Campus 3",
-        image: "campus3.jpg",
-        population: 2,
-        id: 3
-    },
-]
-
-let studentsArray=[
-    {
-        name: "Belinda",
-        id: 1
-    },
-    {
-        name: "Josh",
-        id: 2
-    },
-    {
-        name: "Stephen",
-        id: 3
-    }
-]
-
-deleteCampus = (campusName, campusArray) => {
-  let index = -1;
-  for (let i = 0; i < campusArray.length; i++) {
-    if (campusArray[i].name == campusName) {
-      index = i;
-    }
-  }
-  if (index > -1) {
-    campusArray.splice(index, 1);
-  }
-  console.log(campusArray);
-}
-
-app.get('/data/students', function(request, response){
-    console.log('Request for student data...');
-    response.json(studentsArray);
-});
-
-app.get('/data/campuses', function(request, response){
-    console.log('Request for campus data...');
-    response.json(campusArray);
-});
-
-app.post('/newcampus/submit', function(request, response){
-  console.log('Post request for a new campus...');
-  campusArray.push(request.body);
-  console.log(campusArray);
-  response.send(request.body);
-})
-
-app.delete('/campuses/delete/:campusname', function(request, response){
-  let campus = decodeURI(request.params.campusname);
-  console.log(request);
-  console.log('Post request to delete a campus: ' + campus);
-  deleteCampus(campus, campusArray);
-  response.send(request.body);
-})
+app.get('/students', db.getStudents)
+app.get('/students/:name', db.getStudentByName)
+app.post('/students', db.addStudent)
+// app.get('/data/campuses', function(request, response){
+//     console.log('Request for campus data...')
+//     response.json(campusArray)
+// })
+//
+// app.post('/newcampus/submit', function(request, response){
+//   console.log('Post request for a new campus...')
+//   campusArray.push(request.body)
+//   console.log(campusArray)
+//   response.send(request.body)
+// })
+//
+// app.delete('/campuses/delete/:campusname', function(request, response){
+//   let campus = decodeURI(request.params.campusname)
+//   console.log(request)
+//   console.log('Post request to delete a campus: ' + campus)
+//   deleteCampus(campus, campusArray)
+//   response.send(request.body)
+// })
 
 app.listen(3000, function() {
-	  console.log('Campus viewer app listening on port 3000!');
-});
+	  console.log('Campus viewer app listening on port 3000!')
+})
---
+++ campus-viewer-back/queries.js
@@ -0,0 +1,75 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'me',
+  host: 'localhost',
+  database: 'students-and-campuses',
+  password: 'password',
+  port: 5432,
+})
+
+
+const getStudents = (request, response) => {
+  pool.query('SELECT * FROM students ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const getStudentByName = (request, response) => {
+  const name = decodeURI(request.params.name)
+  pool.query('SELECT * FROM students WHERE name = $1', [name], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const addStudent = (request, response) => {
+  const { name, campus, gpa, image } = request.query
+  console.log(request.query)
+  pool.query('INSERT INTO students (name, campus, gpa, image) VALUES ($1, $2, $3, $4)', [name, campus, gpa, image], (error, results) => {
+    if (error) {
+      throw error
+    }
+    console.log(results)
+    response.status(201).send(`User added with ID (This doesn't work properly): ${}`) // 
+  })
+}
+//
+// const updateUser = (request, response) => {
+//   const id = parseInt(request.params.id)
+//   const { name, email } = request.body
+//
+//   pool.query(
+//     'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+//     [name, email, id],
+//     (error, results) => {
+//       if (error) {
+//         throw error
+//       }
+//       response.status(200).send(`User modified with ID: ${id}`)
+//     }
+//   )
+// }
+//
+// const deleteUser = (request, response) => {
+//   const id = parseInt(request.params.id)
+//
+//   pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     response.status(200).send(`User deleted with ID: ${id}`)
+//   })
+// }
+
+module.exports = {
+  getStudents,
+  getStudentByName,
+  addStudent,
+  // updateUser,
+  // deleteUser,
+}
